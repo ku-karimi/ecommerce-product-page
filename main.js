7257f58b37ed88a6045d7ec8c665a565c0206dd5
ku-karimi <karimi.z8346@gmail.com>
2025-02-23 16:59:24 +0330

    add modal to html and js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// menu for phone
 const menu = document.getElementById("menu");
 const listNav = document.getElementById("list-nav") ;
 let open = false ;
@@ -10,4 +11,54 @@ menu.addEventListener("click" , function () {
         listNav.style.left = "-1000px" ;
         open = false ;
     }
+})
+
+// count number or buy
+
+const plus = document.getElementById("plus") ;
+const mines = document.getElementById("mines") ;
+const number = document.getElementById("number") ;
+
+plus.addEventListener("click" , function () {
+    let num = Number(number.innerHTML) ;
+    number.innerHTML = num + 1 ; 
+})
+
+mines.addEventListener("click" , function () {
+    let num = Number(number.innerHTML) ;
+    if(num > 0 ){ 
+        number.innerHTML = num - 1 ;
+    } 
+})
+
+// modal box
+const modalMain = document.getElementById("modal-main") ;
+const addBtn = document.getElementsByClassName("btn") ;
+const modalBasket = document.getElementById("modal-basket") ;
+const card = document.getElementById("modal") ;
+let modalBox = false ;
+
+modalBasket.addEventListener("click" , function() {
+    if (!modalBox) {
+        card.style.display = "block" ;
+        modalBox = true ;
+    }else{
+        card.style.display = "none" ;
+        modalBox = false ;
+    }
+})
+
+//add to modal
+let modalObj = ["Fall Limited Edition Sneakers"] ;
+let exist ;
+
+addBtn[0].addEventListener("click" , function() {
+    const nameTag = document.getElementsByTagName("h1")[0] ;
+    for(let x of modalObj){
+        if(x == nameTag.innerHTML){
+            exist = x ;
+            break ;
+        }
+    }
+    console.log(exist)
 })
